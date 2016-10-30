545d00b7c49091b38e8fe31d531c38f7cea2f22f
Kaelan Cooter <dcgamers@gmail.com>
2016-10-29 22:59:08 -0400

    terrain contour arrows on edges, created Edge object

--- src/components/Map.js
+++ src/components/Map.js
@@ -42,11 +42,43 @@ function drawEdge(ctx, p1, p2, width = 1, style = 'black') {
   ctx.closePath();
 }
 
+function drawArrow(ctx, fromx, fromy, tox, toy, r = 10){
+	let x_center = tox;
+	let y_center = toy;
+
+	let angle;
+	let x;
+	let y;
+
+	ctx.beginPath();
+
+	angle = Math.atan2(toy - fromy, tox - fromx)
+	x = 2 * r * Math.cos(angle) + x_center;
+	y = 2 * r * Math.sin(angle) + y_center;
+
+	ctx.moveTo(x, y);
+
+	angle += (1 / 3) * (2 * Math.PI)
+	x = r * Math.cos(angle) + x_center;
+	y = r * Math.sin(angle) + y_center;
+
+	ctx.lineTo(x, y);
+
+	angle += (1 / 3) * (2 * Math.PI)
+	x = r * Math.cos(angle) + x_center;
+	y = r * Math.sin(angle) + y_center;
+
+	ctx.lineTo(x, y);
+	ctx.closePath();
+	ctx.fill();
+}
+
 
 function renderMap(canvas, settings = {}) {
   settings = Object.assign({}, {
     width: 500,
     heigth: 500,
+    drawElevationArrows: false,
     drawCell: true,
     drawEdges: true,
     drawNeighborNetwork: true,
@@ -93,16 +125,19 @@ function renderMap(canvas, settings = {}) {
   const minHeight = smallerHeightmap.grid.min()
   const maxHeight = smallerHeightmap.grid.max()
 
-  function getHeight(x, y) {
+  const getHeightAtPoint = _.memoize(function getHeightAtPoint(point) {
     return smallerHeightmap.get(
-      Math.floor(x / (settings.width / HEIGHTMAP_SIZE * 2)),
-      Math.floor(y / (settings.height / HEIGHTMAP_SIZE * 2))
+      Math.floor(point.x / (settings.width / HEIGHTMAP_SIZE * 2)),
+      Math.floor(point.y / (settings.height / HEIGHTMAP_SIZE * 2))
     );
-  }
-  const getHeightMemoized = _.memoize(getHeight);
+  });
 
-  function getColorFor(x, y) {
-    const h = getHeightMemoized(x, y);
+  const getCellHeight = _.memoize(function getHeight(x, y) {
+    return getHeightAtPoint(new Point(x, y));
+  });
+
+  const getColorForCell = _.memoize(function getColorForCell(x, y) {
+    const h = getCellHeight(x, y);
     let color;
     if (h < seaLevelHeight) {
       color = [54, 54, 97];
@@ -118,8 +153,7 @@ function renderMap(canvas, settings = {}) {
       color = [211, 222, 210];
     }
     return color;
-  }
-  const getColorForMemoized = _.memoize(getColorFor);
+  });
 
   function colorToRGB (color) {
     return `rgb(${color.map(c => c.toString()).join(', ')})`;
@@ -127,8 +161,8 @@ function renderMap(canvas, settings = {}) {
 
   if (settings.drawCell) {
     diagram.cells.forEach((cell, index) => {
-      const color = getColorForMemoized(cell.site.x, cell.site.y)
-      // let h = getHeightMemoized(cell.site.x, cell.site.y);
+      const color = getColorForCell(cell.site.x, cell.site.y)
+      // let h = getCellHeight(cell.site.x, cell.site.y);
       // h = parseInt(((h - minHeight) / maxHeight) * 255, 10)
       // const color = [h, h, h];
       ctx.fillStyle = colorToRGB(color);
@@ -150,13 +184,13 @@ function renderMap(canvas, settings = {}) {
   // draw voronoi edges
   if (settings.drawEdges) {
     diagram.edges.forEach(edge => {
-      const leftHeight = getHeightMemoized(edge.lSite.x, edge.lSite.y);
-      const rightHeight = edge.rSite ? getHeightMemoized(edge.rSite.x, edge.rSite.y) : Infinity;
+      const leftHeight = getCellHeight(edge.lSite.x, edge.lSite.y);
+      const rightHeight = edge.rSite ? getCellHeight(edge.rSite.x, edge.rSite.y) : Infinity;
       let color;
 
       // color lines between water cells like the cell color
       if (leftHeight < seaLevelHeight && rightHeight < seaLevelHeight) {
-        color = colorToRGB(getColorForMemoized(edge.lSite.x, edge.rSite.y));
+        color = colorToRGB(getColorForCell(edge.lSite.x, edge.rSite.y));
       } else {
         color = '#333';
       }
@@ -222,12 +256,85 @@ function renderMap(canvas, settings = {}) {
       ctx.fillStyle = 'rgba(200, 200, 200, 0.75)';
       ctx.textAlign = "center";
       ctx.fillText(
-        _.round(getHeightMemoized(cell.site.x, cell.site.y), 1),
+        _.round(getCellHeight(cell.site.x, cell.site.y), 1),
         cell.site.x,
         cell.site.y
       );
     });
   }
+
+  class Edge {
+    constructor(edge) {
+      this.edge = edge;
+    }
+  }
+  let edges = [];
+
+  diagram.edges.forEach((edge, index) => {
+    const edgeObj = new Edge(edge);
+    edges[index] = edgeObj;
+  });
+
+  class Corner {
+    constructor(point, index) {
+      this.point = point;
+      this.edgeIndex = index;
+      this.height = getHeightAtPoint(point);
+    }
+  }
+
+  let corners = [];
+  let cornersByPoint = {};
+  diagram.edges.forEach((edge, index) => {
+    // drawDot(ctx, new Point(edge.va.x, edge.va.y), 'yellow');
+    const cornerFrom = new Corner(new Point(edge.va.x, edge.va.y), index);
+    corners.push(cornerFrom);
+    edges[index].from = cornerFrom;
+    if (!cornersByPoint[edge.va.x]) {
+      cornersByPoint[edge.va.x] = {};
+    }
+    cornersByPoint[edge.va.x][edge.va.y] = cornerFrom;
+  });
+  diagram.edges.forEach((edge, index) => {
+    const cornerTo = cornersByPoint[edge.vb.x][edge.vb.y];
+    if (!cornerTo) { // broken edge
+      return
+    }
+    edges[index].to = cornerTo;
+    const cornerFrom = edges[index].from;
+
+    if (cornerFrom.height < cornerTo.height) {
+      edges[index].up = cornerTo;
+      edges[index].down = cornerFrom;
+    } else {
+      edges[index].up = cornerFrom;
+      edges[index].down = cornerTo;
+    }
+
+    edges[index].center = new Point(
+      (edges[index].from.point.x + edges[index].to.point.x) / 2,
+      (edges[index].from.point.y + edges[index].to.point.y) / 2
+    );
+  });
+  // clean up broken edges
+  edges = edges.filter(edge => Object.keys(edge).length > 2);
+
+  if (settings.drawElevationArrows) {
+    ctx.fillStyle = 'black';
+    edges.forEach(edge => {
+      if (getHeightAtPoint(edge.center) >= seaLevelHeight) {
+        drawArrow(
+          ctx,
+          edge.up.point.x,
+          edge.up.point.y,
+          edge.center.x,
+          edge.center.y,
+          3
+        );
+      }
+    });
+  }
+  console.log(edges);
 }
 
 
